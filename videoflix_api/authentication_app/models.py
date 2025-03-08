21e5af7b6af3f7fd01e3cf54dfb9840c4fe8344e
Bladers123 <tristangehring123@hotmail.de>
2025-03-08 20:39:08 +0100

    forms for admin panel

--- videoflix_api/authentication_app/models.py
+++ videoflix_api/authentication_app/models.py
@@ -2,4 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class CustomUser(AbstractUser):
-    type = models.CharField(max_length=255)
+    custom = models.CharField(max_length=500, default='')
+    address = models.CharField(max_length=150, default='')
+    phone = models.CharField(max_length=25, default='')
