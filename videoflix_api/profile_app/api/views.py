c872aeb1643a3f19c857091dd08394504bb5936c
Bladers123 <tristangehring123@hotmail.de>
2025-03-23 13:33:38 +0100

    get subprofiles by profile

--- videoflix_api/profile_app/api/views.py
+++ videoflix_api/profile_app/api/views.py
@@ -12,9 +12,17 @@ class ProfileViewSet(viewsets.ModelViewSet):
     serializer_class = ProfileSerializer
 
 
+
+
 class SubProfileViewSet(viewsets.ModelViewSet):
     queryset = SubProfile.objects.all()
     permission_classes = [IsAuthenticated]
     serializer_class = SubProfileSerializer
 
-    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        profile_id = self.request.query_params.get('profile', None)
+        if profile_id is not None:
+            queryset = queryset.filter(profile=profile_id)
+        return queryset
+
