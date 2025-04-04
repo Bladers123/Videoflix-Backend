88e73208c0f8dbb62440841e3f48f7e0c2966f48
Bladers123 <tristangehring123@hotmail.de>
2025-04-04 22:16:53 +0200

    video request

--- videoflix_api/video_app/api/urls.py
+++ videoflix_api/video_app/api/urls.py
@@ -4,5 +4,6 @@ from .views import DownloadVideoView, VideoTestView
 
 urlpatterns = [
     path('', VideoTestView.as_view(), name='video-test'),
+    path('<str:video_name>/<str:file_name>/', DownloadVideoView.as_view()),
     path('<str:video_name>/', DownloadVideoView.as_view(), name='stream_video'),
 ]
--- videoflix_api/video_app/api/views.py
+++ videoflix_api/video_app/api/views.py
@@ -8,9 +8,19 @@ from core.ftp_client import FTPClient
 
 
 class DownloadVideoView(APIView):
-    def get(self, request, video_name):
-        # Hier gehen wir davon aus, dass das HLS-Manifest mit der Endung .m3u8 vorliegt
-        remote_path = f'/videos/{video_name}'
+    def get(self, request, video_name, file_name=None):
+        # Falls kein file_name übergeben wird, liefern wir das Manifest
+        if file_name is None:
+            remote_path = f'/videos/{video_name}/{video_name}.m3u8'
+            content_type = 'application/vnd.apple.mpegurl'
+        else:
+            remote_path = f'/videos/{video_name}/{file_name}'
+            # MIME-Typ anhand der Dateiendung festlegen
+            if file_name.endswith('.ts'):
+                content_type = 'video/MP2T'
+            else:
+                content_type = 'application/octet-stream'
+        print(remote_path)
         try:
             ftp_client = FTPClient()
             buffer = ftp_client.download_file_to_buffer(remote_path)
@@ -18,10 +28,7 @@ class DownloadVideoView(APIView):
         except Exception as e:
             raise Http404(f"Fehler beim Abrufen der Datei: {e}")
         
-        # Für m3u8-Dateien wird als MIME-Typ 'application/vnd.apple.mpegurl' genutzt
-        response = StreamingHttpResponse(buffer, content_type='application/vnd.apple.mpegurl')
-        # Wenn der Stream direkt im Browser abgespielt werden soll, ist Content-Disposition in der Regel nicht nötig.
-        return response
+        return StreamingHttpResponse(buffer, content_type=content_type)
 
 
 
