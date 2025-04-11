4b0196314da1103a81dfff0c27e2a7179beb0d7a
Bladers123 <tristangehring123@hotmail.de>
2025-04-11 21:40:08 +0200

    funktioniert noch

--- videoflix_api/video_app/models.py
+++ videoflix_api/video_app/models.py
@@ -3,11 +3,23 @@ from django.db import models
 from django.conf import settings
 
 class Video(models.Model):
+    VIDEO_TYPE_CHOICES = [
+        ('movie', 'Movie'),
+        ('clip', 'Clip'),
+    ]
+
+    
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     video_file = models.FileField(upload_to='videos/')
     thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    video_type = models.CharField(
+        max_length=10,
+        choices=VIDEO_TYPE_CHOICES,
+        default='movie',
+        help_text="Bitte auswählen: movie oder clip"
+    )
 
     def __str__(self):
         return self.title
