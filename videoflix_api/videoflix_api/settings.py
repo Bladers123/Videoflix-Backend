37c7bd43f077baa3339c90f7bf6d0a8f169713e9
Bladers123 <tristangehring123@hotmail.de>
2025-05-29 17:08:03 +0200

    fix settings.py

--- videoflix_api/videoflix_api/settings.py
+++ videoflix_api/videoflix_api/settings.py
@@ -20,20 +20,24 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
+
 # Allowed Hosts
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+_hosts = os.getenv('ALLOWED_HOSTS', '')
+ALLOWED_HOSTS = [h.strip() for h in _hosts.split(',') if h.strip()]
+
 
 
 CSRF_TRUSTED_ORIGINS = [
     'https://videoflix-api.tristan-gehring.com',
 ]
+
 
 # Application definition
 
